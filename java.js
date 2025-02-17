bf7bbc6a2fbecdb8ea2249df340cd5d0d977a4b7
Dear-deer1 <alenazlobina112@gmail.com>
2025-02-17 09:36:06 +0300

    последнее изменение

--- java.js
+++ java.js
@@ -1,14 +1,63 @@
-let count = 0;
-const button = document.getElementById("clickButton");
-const counterDisplay = document.getElementById("counter");
-const message = document.getElementById("message");
+document.addEventListener("DOMContentLoaded", function(){
 
-button.addEventListener("click", function() {
-    count++;
-    counterDisplay.textContent = count;
-    
-    if (count >= 15) {
-        button.disabled = true;
-        message.textContent = "Вы достигли максимального количества кликов!";
+    let count = 0;
+    const but = document.getElementById("but");
+    const click = document.getElementById("click");
+
+    but.addEventListener ("click", function(){
+ 
+        count += 1;
+        click.textContent = count;
+
+        if(count >= 8){
+            but.disabled = true;
+        }
+});
+
+
+let co = 0;
+const tim = document.getElementById ("timer");
+const interval = setInterval( function (){
+    co +=1;
+    tim.textContent = co;
+
+    if(co >=10){
+        clearInterval(interval);
     }
+
+}, 1000);
+
+let ct = 0;
+let intervalic; 
+
+const tm = document.getElementById ("tm");
+
+const sb = document.getElementById ("sb");
+const st = document.getElementById ("st");
+const res = document.getElementById ("res");
+
+sb.addEventListener("click", function(){
+if(!intervalic){
+    intervalic = setInterval (function(){
+        ct++;
+        tm.textContent = ct;
+    }, 1000);
+};
+});
+
+st.addEventListener ("click", function(){
+    clearInterval (intervalic);
+    intervalic = null;
+});
+
+
+res.addEventListener ("click", function(){
+clearInterval  (intervalic);
+intervalic = null;
+ct = 0;
+tm.textContent = ct;
+
+});
+
+
 });
